f5ea2217d37a371a4c76134a3f139af7bc7056a3
Pedro Zaccaria <pedrozaccaria@gmail.com>
2022-06-06 00:34:46 -0300

    code53 added

---
+++ lp-lab-2/code53/code53.c
@@ -0,0 +1,48 @@
+/*
+    Matricula - [hidden]
+    Nome completo - Pedro Henrique Castro Zaccaria Rodrigues
+    Curso - Bacharelado em Ciencia e Tecnologia
+    Semestre - 1.
+
+    Exercise: Verifies if a given number is prime.
+    (2/3) while
+ */
+
+// Preprocessing directives
+#include <stdio.h>
+#include <stdbool.h>
+
+// Functions prototypes
+bool isPrime(int value);
+
+// Main function
+int main(void){
+    int num;
+    // Input validation (num > 0)
+    printf("\nInsert the value of num:\n");
+    while (1) {
+        scanf("%d", &num);
+        if (num > 0) break;
+        printf("\nPlease enter a value greater than zero:\n");
+    }
+
+    isPrime(num) ? printf("Is prime.") : printf("Isn't prime.");
+
+    return 0;
+}
+
+// Other functions
+bool isPrime(int value){
+    int i = 1, aux = 0;
+    while (i <= value) {
+        if (value % i == 0)
+            aux++;
+
+        if (aux > 2)
+            return 0;
+
+        i++;
+    }
+
+    return (aux == 2) ? 1 : 0;
+}
